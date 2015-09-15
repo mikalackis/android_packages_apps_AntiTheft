3cd9eb6349181993caa0c10279cf95316b574387
Mikalacki Sava <mikalackis@gmail.com>
2015-09-15 22:52:22 +0200

    work work
    Change-Id: I3bf1322e0ea56d435c7eb98e64a5b01e5bc2d8f5

--- src/com/android/antitheft/services/AntiTheftService.java
+++ src/com/android/antitheft/services/AntiTheftService.java
@@ -4,7 +4,7 @@ package com.android.antitheft.services;
 import android.app.Service;
 import android.content.Context;
 
-public abstract class AntiTheftService extends Service{
+public abstract class AntiTheftService extends Service {
 
     public abstract void startAntiTheftService(final Context context, final int state);
 
